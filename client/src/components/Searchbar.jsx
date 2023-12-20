a5de1ba97869e508412b5e7fc454bf5bdcfac087
mauricea11 <mauriceaugust117@gmail.com>
2023-12-19 20:42:30 -0500

    Fixed placement of search bar and search icon

--- client/src/components/Searchbar.jsx
+++ client/src/components/Searchbar.jsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Searchbar.css";
 import { FaSearch } from "react-icons/fa";
 
 function Searchbar() {
+  const [input, setInput] = useState("");
   return (
     <div className="inputwrapper">
       <FaSearch id="search-icon" />
-      <input type="text" placeholder="Type something..." />
+      <input
+        type="text"
+        placeholder="Type something..."
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
     </div>
   );
 }
