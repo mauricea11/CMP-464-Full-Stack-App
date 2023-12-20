8e53a9f34c0ca8657a3622fde8f6d78099945bba
mauricea11 <mauriceaugust117@gmail.com>
2023-12-19 20:49:20 -0500

    Get data and input change abstraction

--- client/src/components/Searchbar.jsx
+++ client/src/components/Searchbar.jsx
@@ -4,6 +4,14 @@ import { FaSearch } from "react-icons/fa";
 
 function Searchbar() {
   const [input, setInput] = useState("");
+
+  // Match input value with user entries by filtering results
+  const handleChange = "";
+
+  // Get list of data
+  const getData = (value) => {
+    console.log(data);
+  };
   return (
     <div className="inputwrapper">
       <FaSearch id="search-icon" />
