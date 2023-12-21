9c7b002817b7ee7ad048fe4da93e3d9ab04d4d29
mauricea11 <mauriceaugust117@gmail.com>
2023-12-20 21:24:49 -0500

    Added api path to backend for getData function

--- client/src/components/Searchbar.jsx
+++ client/src/components/Searchbar.jsx
@@ -1,17 +1,26 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Searchbar.css";
 import { FaSearch } from "react-icons/fa";
 
 function Searchbar() {
   const [input, setInput] = useState("");
+  const [data, setData] = useState([]);
+
+  const url = "http://localhost:3000/data";
 
   // Match input value with user entries by filtering results
   const handleChange = "";
 
   // Get list of data
-  const getData = (value) => {
-    console.log(data);
-  };
+  useEffect(() => {
+    async function getData() {
+      const resp = await fetch(url);
+      const jsondata = await resp.json();
+      setData(jsondata);
+    }
+
+    getData();
+  }, []);
   return (
     <div className="inputwrapper">
       <FaSearch id="search-icon" />
